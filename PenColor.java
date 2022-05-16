6c575ecc76a96005f02c42cf2b27eaec8ed0776e
Javkhlanbaatar <99939478+Javkhlanbaatar@users.noreply.github.com>
2022-05-16 20:55:41 +0800

    Add files via upload
    turtle

---
+++ PenColor.java
@@ -0,0 +1,25 @@
+/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package turtle;
+
+import java.awt.Color;
+import java.util.Random;
+
+/**
+ * Enumeration of turtle pen colors.
+ * 
+ * You may not modify this enumeration.
+ */
+public enum PenColor {
+    BLACK,
+    GRAY,
+    RED,
+    PINK,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    CYAN,
+    BLUE,
+    MAGENTA;
+}
